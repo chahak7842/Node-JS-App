a2f6f3d4f5d3e9719957fdae994a42fc298662fe
Mohit Khurana <mohit.khurana@salesforce.com>
2022-01-24 12:17:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+var client = require('smartsheet');
+const { Pool,Client } = require('pg');
+const dbRoute = require('./Routes/db')
+global.smartsheetRow ="";
+app.get('/', (req, res) => {
+ 
+
+var smartsheet = client.createClient({accessToken:'wqcp4Jr9wKsaUfajS0DIxkSrd3qMH3YitxSCE'});
+
+var options = {
+  sheetId: 4067414670370692, // Id of Sheet
+  rowId: 3743601462798212
+};
+
+smartsheet.sheets.getRow(options)
+  .then(function(row) {
+    console.log(row);
+    //
+    res.send(row)
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+// Get sheet
+
+    
+   
+});
+ 
+// Start the server
+
+
+app.use("/db",dbRoute)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
