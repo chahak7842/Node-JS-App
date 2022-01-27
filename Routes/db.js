b279b82b512bbbaa336757f284b9a6c9470a431a
Mohit Khurana <mohit.khurana@salesforce.com>
2022-01-27 11:15:26 +0530

    review

--- Routes/db.js
+++ Routes/db.js
@@ -13,9 +13,9 @@ function initializeSmartsheetClient(token, logLevel) {
     });
 }
 const config = {
-  user: 'nkdtpihfbhjdjk',
-  database: 'd880or9b563jki',
-  password: 'f0e1b155c789bb55e992293f71035cc3a9fe6fce5fb3a04e923cd24687b539d4',
+  user: 'bwrzkjxunkgmmf',
+  database: 'd7ndg2j0guu48t',
+  password: '27297b65bc871cd8fbb337898f4b1136d2622f2121d1a3c45a6328b116b3fd0a',
   port: 5432,
   connectionString: process.env.HEROKU_POSTGRESQL_NAVY_URL,
   ssl: { rejectUnauthorized: false }                 //Default port, change it if needed
@@ -24,39 +24,42 @@ var pool = new pg.Pool(config)
 
 function updatePostgress(arr) {
   pool.connect(function (err, conn, done) {
-    // watch for any connect issues
-    if (err) console.log(err);
-    
-    conn.query(
-        //'INSERT INTO test_table (id, casenumber, status) VALUES %L', values,
-        'INSERT INTO student ("CaseNumber", "Status") VALUES ($1,$2);', arr,
-        function(err, result) {
-          console.log('Insertion result',result.rows.sfid)
-            //res.json(result.rows);
-            if (err != null || result.rowCount != 0) {
-              conn.query('SELECT * FROM student',
-              function(err, result) {
-                done();
-                if (err) {
+      // watch for any connect issues
+      if (err) console.log(err);
+      var values = [
+        [1, 0001, 'Open'], 
+        [2, 0002, 'Closed']
+      ];
+      conn.query(
+          //'INSERT INTO test_table (id, casenumber, status) VALUES %L', values,
+          'INSERT INTO salesforce.case ("subject", "status") VALUES ($1,$2);', arr,
+          function(err, result) {
+            console.log('Insertion result',result.rows.sfid)
+              //res.json(result.rows);
+              if (err != null || result.rowCount != 0) {
+                conn.query('SELECT * FROM salesforce.case',
+                function(err, result) {
+                  done();
+                  if (err) {
 
-                    res.status(400).json({error: err.message});
-                }
-                else {
-                    // this will still cause jquery to display 'Record updated!'
-                    // eventhough it was inserted
-                    //res.json(result.rows);
-                    console.log('select result',result.rows)
-                    //res.send(result);
-                }
-              });
-            }
-            else {
-                done();
-                res.json(result);
-            }
-        }
-    );
-});
+                      res.status(400).json({error: err.message});
+                  }
+                  else {
+                      // this will still cause jquery to display 'Record updated!'
+                      // eventhough it was inserted
+                      //res.json(result.rows);
+                      console.log('select result',result.rows)
+                      //res.send(result);
+                  }
+                });
+              }
+              else {
+                  done();
+                  res.json(result);
+              }
+          }
+      );
+  });
 }
 //app.set('port', process.env.PORT || 8080);
 app.get('/', function(req, res) {
